23927b7bb5d8a2792411c43a6f7a0ec3325c15db
hari122341 <hariharanmanikanta999@gmail.com>
2025-06-03 22:26:36 +0530

    inital setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Page = () => {
+  return (
+    <div className='text-2xl underline'>
+      Welcome to my Sass App
+      <Button>This is a button</Button>
+
+    </div>
+  )
+}
+
+export default Page
