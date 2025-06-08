dbe0e394990c30a35c849f2f35be4dfd2cdf077c
hari122341 <hariharanmanikanta999@gmail.com>
2025-06-09 02:25:47 +0530

    added env

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
       { hostname: 'img.clerk.com' }
 
     ]
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!
   }
 };
 
